7896330e0b4eecb4824e2668a1827c86f446f913
TarasMysiura <tarasmysiura@gmail.com>
2023-07-27 14:01:30 +0300

    add

--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -1,82 +1,95 @@
-import React from 'react';
-import {
-  Button,
-  Form,
-  Input,
-  Label,
-  Span,
-  TitleH2,
-} from './PhonebookForm.styled';
+import React, { useState } from 'react';
+import css from './PhonebookForm.module.css';
+
+import { Formik, Form, Field } from 'formik';
+
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+
+const initialValues = {
+  name: '',
+  number: '',
+};
 
 export const PhonebookForm = ({ title, onAddContact }) => {
-  const { name, number } = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-
-  // const [name, setName] = useState('');
-  // const [number, setNumber] = useState('');
+  const [name, setName] = useState(initialValues.name);
+  const [number, setNumber] = useState(initialValues.number);
+  // const initialValues = {
+  //   name: name,
+  //   number: number,
+  // };
 
   const handleInputChange = event => {
     const { name, value } = event.currentTarget;
     switch (name) {
       case 'name':
-        dispatch({ type: 'name', paiload: value });
+        setName(value);
         break;
       case 'number':
-        dispatch({ type: 'number', paiload: value });
+        setNumber(value);
         break;
       default:
         break;
     }
   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-
+  const handleSubmit = (values, {resetForm}) => {
+    // event.preventDefault();
+    
     const contactData = {
       name,
       number,
     };
+    console.log('contactData: ', contactData);
+    
+    console.log('values: ', values);
 
     onAddContact(contactData);
 
-    dispatch({ type: 'name', paiload: '' });
-    dispatch({ type: 'number', paiload: '' });
+    // setName('');
+    // setNumber('');
   };
 
   return (
     <>
-      <TitleH2>{title}</TitleH2>
-      <Form onSubmit={handleSubmit}>
-        <Label>
-          <Span>Name</Span>
-          <Input
-            onChange={handleInputChange}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            value={name}
-            autoComplete="true"
-            required
-          />
-        </Label>
-        <Label>
-          <Span>Number</Span>
-          <Input
-            onChange={handleInputChange}
-            value={number}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </Label>
-        <Button type="submit">Add contact</Button>
-      </Form>
+      <h2 className={css.title}>{title}</h2>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        onChange={handleInputChange}
+      >
+        <Form className={css.formStyle}>
+          <label className={css.label}>
+            <span className={css.span}>Name</span>
+            <Field
+              className={css.input}
+              onChange={handleInputChange}
+              type="text"
+              name="name"
+              // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              value={name}
+              autoComplete="true"
+              required
+            />
+          </label>
+          <label className={css.label}>
+            <span className={css.span}>Number</span>
+            <Field
+              className={css.input}
+              onChange={handleInputChange}
+              value={number}
+              type="tel"
+              name="number"
+              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit" className={css.button}>
+            Add contact
+          </button>
+        </Form>
+      </Formik>
     </>
   );
 };
